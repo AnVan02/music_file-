bf6e38b8f8112ed24faa0d3734b96a2ac185ccf5
Văn An <89497878+AnVan02@users.noreply.github.com>
2024-06-23 14:17:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow } = require('electron');
+
+function createWindow(){
+     const win = new BrowserWindow({
+          width: 480,
+          height: 500,
+          webPreferences:{ 
+               NodeIntegration:true 
+     },
+     resizable:false
+     
+          
+     });
+     win.loadFile('index.html');
+
+}
+app.on('ready',createWindow);
---
+++ player.js
@@ -0,0 +1,12 @@
+Amplitude.init({
+     songs:[
+          {
+               name:'Pháo Hoa',
+               artist:'Dreamer; Phí Phương Anh; RIN9; MiiNa',
+               album:'Nhạc Việt Nam, Nhạc pop',
+               url:'./music/Phao Hoa - Dreamer_ Phi Phuong Anh_ RIN9.mp3',
+               cover_art_url:'./music/PhaoHoa.jpg',
+
+          }
+     ]
+});
